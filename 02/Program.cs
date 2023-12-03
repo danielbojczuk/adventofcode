d45b7754ce089855bcfbf16824733e61e0a5f282
Daniel Bojczuk <daniel.fernandes.bojczuk@rabobank.nl>
2023-12-03 08:42:48 +0100

    Part2

---
+++ 02/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.RegularExpressions;
+
+
+
+var games = File.ReadAllLines("input.txt");
+
+//Part1
+var totalRed = 12;
+var totalGreen = 13;
+var totalBlue = 14;
+
+var idSum = 0;
+var power = 0;
+foreach (var game in games)
+{
+    var gameInfo = game.Split(':');
+    if(!int.TryParse(gameInfo[0].Split(" ")[1], out var id))
+    {
+        throw new InvalidOperationException("Couldn't retrieve id");
+    }
+
+    var rounds = gameInfo[1].Split(";");
+
+    var minimumCubes = new Dictionary<string, int>()
+    {
+        { "green",0 },
+        { "blue",0 },
+        { "red",0 }
+    };
+
+    var valid = true;
+    foreach (var round in rounds)
+    {
+        var totalCubes = new Dictionary<string, int>()
+        {
+            { "green",0 },
+            { "blue",0 },
+            { "red",0 }
+        };
+
+        var cubesSet = round.Split(",");
+        foreach (var cubes in cubesSet)
+        {
+            var cubesInfo = cubes.Trim().Split(" ");
+            if (!int.TryParse(cubesInfo[0], out var qtd))
+            {
+                throw new InvalidOperationException("Couldn't retrieve qtd");
+            }
+            totalCubes[cubesInfo[1]] += qtd;
+
+            if(qtd > minimumCubes[cubesInfo[1]]) {
+                minimumCubes[cubesInfo[1]] = qtd;
+            }
+        }
+        if (totalCubes["red"] > totalRed || totalCubes["blue"] > totalBlue || totalCubes["green"] > totalGreen)
+        {
+            valid = false;
+        }
+    }
+    //part1
+    if(valid)
+    {
+        idSum += id;
+    }
+
+    //part2
+    var minimumRed = 1;
+    var minimumGreen = 1;
+    var minimumBlue = 1;
+
+    if (minimumCubes["red"] != 0)
+    {
+        minimumRed = minimumCubes["red"];
+    }
+
+    if (minimumCubes["green"] != 0)
+    {
+        minimumGreen = minimumCubes["green"];
+    }
+
+    if (minimumCubes["blue"] != 0)
+    {
+        minimumBlue = minimumCubes["blue"];
+    }
+    power += minimumRed * minimumGreen * minimumBlue;
+}
+//part1
+Console.WriteLine(idSum);
+
+//part2
+Console.WriteLine(power);
+
---
+++ 2023/01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+
+var stringNumbers = new Dictionary<string, int>
+{
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 }
+};
+
+
+
+var getValue = (string input, int position) => 
+{
+    if (int.TryParse(input[position].ToString(), out int digit))
+    {
+        return digit;
+    } 
+    foreach (var number in stringNumbers.Keys)
+    {
+        if(position+number.Length > input.Length)
+        {
+            continue;
+        }
+        var possibleNumber = input.Substring(position, number.Length);
+        if(possibleNumber == number)
+        {
+            return stringNumbers[number];
+        }
+    }
+    return -1;
+};
+
+
+//Part 1
+var input = File.ReadAllLines("input.txt");
+int total = 0;
+for(int l = 0; l < input.Length; l++)
+{
+    int firstDigit = -1;
+    int lastDigit = -1;
+    for (int i = 0; i < input[l].Length; i++)
+    {
+        var digit = getValue(input[l], i);
+        if (digit != -1)
+        {
+            if (firstDigit == -1)
+            {
+                firstDigit = digit;
+            }
+            lastDigit = digit;
+        }
+    }
+    if(int.TryParse($"{firstDigit}{lastDigit}", out int result))
+    {
+        total += result;
+    }
+    Console.WriteLine(total);
+}
+Console.WriteLine(total);
+Console.ReadLine();
